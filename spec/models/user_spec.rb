a3ee8617db44a4e55a57f524f4de35928f4ee970
Githerdone <GitHub@JakeDanforth.com>
2013-02-16 20:23:54 -0700

    Finish user edit, update, index, and destroy actions chapter nince

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,11 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 require 'spec_helper'
 
 describe User do
-
-  	before { @user = User.new(name: "Example User", email: "user@example.com", 
+  before { @user = User.new(name: "Example User", email: "user@example.com", 
   							password: "foobar", password_confirmation: "foobar") }
 
 
-  	subject { @user }
+  subject { @user }
 
 	  it { should respond_to(:name) }
   	it { should respond_to(:email) }
@@ -27,6 +15,22 @@ describe User do
     it { should respond_to(:remember_token) }
     it { should respond_to(:authenticate) }  
     it { should be_valid }
+
+    it { should respond_to(:admin) }
+    it { should respond_to(:authenticate) }
+
+    it { should be_valid }
+    it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
 
   	describe "when name is not present" do
     	before { @user.name = " " }
