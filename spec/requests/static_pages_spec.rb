99f10e98bd9d69fe2a2c7fe2b7a89df4f4c465ae
Githerdone <GitHub@JakeDanforth.com>
2013-02-17 22:04:52 -0700

    middle of chapt 11

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,10 +4,10 @@ describe "Static pages" do
 
   subject { page }
 
-   shared_examples_for "all static pages" do
-    it { should have_selector('h1',    text: heading) }
-    it { should have_selector('title', text: full_title(page_title)) }
-   end
+      shared_examples_for "all static pages" do
+        it { should have_selector('h1',    text: heading) }
+        it { should have_selector('title', text: full_title(page_title)) }
+      end
 
 
       describe "Home page" do
@@ -21,22 +21,32 @@ describe "Static pages" do
       
         describe "for signed-in users" do
           let(:user) { FactoryGirl.create(:user) }
-          before do
-            FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-            FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-            sign_in user
-            visit root_path
-          end
-        
-        
+            before do
+              FactoryGirl.create(:micropost, user: user, content: "Lorem")
+              FactoryGirl.create(:micropost, user: user, content: "Ipsum")
+              sign_in user
+              visit root_path
+            end
+
           it "should render the user's feed" do
             user.feed.each do |item|
               page.should have_selector("li##{item.id}", text: item.content)
             end
           end
-        end    
-      end
+        end
       
+        describe "follower/following counts" do
+          let(:other_user) { FactoryGirl.create(:user) }
+            before do
+              other_user.follow!(user)
+              visit root_path
+            end
+
+          #it { should have_link("0 following", href: following_user_path(user)) }
+          #it { should have_link("1 followers", href: followers_user_path(user)) }
+        end
+      end   
+          
       describe "Help page" do
         before { visit help_path }
         
@@ -67,17 +77,17 @@ describe "Static pages" do
         it { should_not have_selector 'title', text: '| Contact page' }
       end
 
-  it "should have the right links on the layout" do
-   visit root_path
-   click_link "About"
-   page.should have_selector 'title', text: full_title('About Us')
-   click_link "Help"
-   page.should have_selector 'title', text: full_title('Help')
-   click_link "Contact"
-   page.should have_selector 'title', text: full_title('Contact')
-   click_link "Home"
-   click_link "Sign up now... Because it's Awesome!!!"
-   #page.should have_selector 'title', text: full_title('Sign Up')
-   click_link "sample app"
-  end
+        it "should have the right links on the layout" do
+          visit root_path
+          click_link "About"
+          page.should have_selector 'title', text: full_title('About Us')
+          click_link "Help"
+          page.should have_selector 'title', text: full_title('Help')
+          click_link "Contact"
+          page.should have_selector 'title', text: full_title('Contact')
+          click_link "Home"
+          click_link "Sign up now... Because it's Awesome!!!"
+          #page.should have_selector 'title', text: full_title('Sign Up')
+          click_link "sample app"
+        end
 end
