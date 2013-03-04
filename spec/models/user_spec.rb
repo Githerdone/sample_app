1fb151161ac91014c11c8a756eec4556e1807d21
Githerdone <GitHub@JakeDanforth.com>
2013-03-03 20:30:52 -0700

    Trying to fix the Follow Unfollow buttons as well as some missing code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,38 +7,38 @@ describe User do
   end
 
 
-    subject { @user }
+  subject { @user }
 
-	  it { should respond_to(:name) }
-  	it { should respond_to(:email) }
-  	it { should respond_to(:password_digest) }
-  	it { should respond_to(:password) }
-  	it { should respond_to(:password_confirmation) }
-    it { should respond_to(:remember_token) }
-    it { should respond_to(:authenticate) }  
-    it { should be_valid }
+	it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
+  it { should be_valid }
 
-    it { should respond_to(:admin) }
-    it { should respond_to(:authenticate) }
-    it { should respond_to(:microposts) }
-    it { should respond_to(:feed) }
-    it { should respond_to(:relationships) }
-    it { should respond_to(:followed_users) }
-    it { should respond_to(:reverse_relationships) }
-    it { should respond_to(:followers) }
-    it { should respond_to(:following?) }
-    it { should respond_to(:follow!) }
-    it { should respond_to(:unfollow!) }
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
     
-    it { should be_valid }
-    it { should_not be_admin }
+  it { should be_valid }
+  it { should_not be_admin }
 
   describe "following" do
-    let(:other_user) { FactoryGirl.create(:user) }    
-      before do
-        @user.save
-        @user.follow!(other_user)
-      end
+  let(:other_user) { FactoryGirl.create(:user) }    
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
 
     it { should be_following(other_user) }
     its(:followed_users) { should include(other_user) }
@@ -49,6 +49,7 @@ describe User do
       it { should_not be_following(other_user) }
       its(:followed_users) { should_not include(other_user) }
     end
+
     describe "followed user" do
       subject { other_user }
       its(:followers) { should include(@user) }
@@ -61,18 +62,33 @@ describe User do
     let!(:older_micropost) do 
       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
     end
+    
     let!(:newer_micropost) do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
     end
     
     describe "status" do
+      
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+    
+     let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
       end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+       
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
   end
 
@@ -83,7 +99,7 @@ describe User do
     end
 
     it { should be_admin }
-  end
+    end
 
 
   	describe "when name is not present" do
@@ -103,22 +119,22 @@ describe User do
 
     describe "when email format is invalid" do
     	it "should be invalid" do
-      		addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+      	addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
-      		addresses.each do |invalid_address|
+      	addresses.each do |invalid_address|
         	@user.email = invalid_address
         	@user.should_not be_valid
-      		end      
+      	end      
     	end
   	end
 
   	describe "when email format is valid" do
     	it "should be valid" do
      		addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-      		addresses.each do |valid_address|
+      	addresses.each do |valid_address|
         	@user.email = valid_address
         	@user.should be_valid
-      		end      
+      	end      
     	end
   	end
 
@@ -128,14 +144,13 @@ describe User do
         user_with_same_email.email = @user.email.upcase
         user_with_same_email.save
       end
-
     	it { should_not be_valid }
     end
 
-  describe "when password is not present" do
-  	before { @user.password = @user.password_confirmation = " " }
-  	it { should_not be_valid }
-	end
+    describe "when password is not present" do
+  	 before { @user.password = @user.password_confirmation = " " }
+  	 it { should_not be_valid }
+	 end
 
 	describe "when password doesn't match confirmation" do
 		before { @user.password_confirmation = "mismatch" }
@@ -148,30 +163,30 @@ describe User do
 	end
 
 	describe "return value of authenticate method" do
-  		before { @user.save }
-  		let(:found_user) { User.find_by_email(@user.email) }
+  	before { @user.save }
+  	let(:found_user) { User.find_by_email(@user.email) }
 
-  		describe "with valid password" do
-    		it { should == found_user.authenticate(@user.password) }
-  		end
+  	describe "with valid password" do
+   		it { should == found_user.authenticate(@user.password) }
+  	end
 
-  		describe "with invalid password" do
-    		let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+  	describe "with invalid password" do
+  	let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-    		it { should_not == user_for_invalid_password }
-    		specify { user_for_invalid_password.should be_false }
-  		end
+    	it { should_not == user_for_invalid_password }
+   		specify { user_for_invalid_password.should be_false }
+  	end
 	end
 
 	describe "with a password that's too short" do
-  		before { @user.password = @user.password_confirmation = "a" * 5 }
-  		it { should be_invalid }
+  	before { @user.password = @user.password_confirmation = "a" * 5 }
+  	it { should be_invalid }
 	end
 
-    describe "remember token" do
-      before { @user.save }
-      its(:remember_token) { should_not be_blank }
-    end
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
   describe "micropost associations" do
     before { @user.save }
